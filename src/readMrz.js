ef1b158772253c7afe68449b2e0365fabc1912ca
Adam Burdett <burdettadam@gmail.com>
2020-11-30 00:53:23 -0700

    build-js. code for better browserify
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- src/readMrz.js
+++ src/readMrz.js
@@ -1,34 +1,24 @@
 'use strict';
 
-const ENVIRONMENT_IS_WEB = typeof window === 'object';
-const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
-
-const { join } = require('path');
+// const { join } = require('path');
 
 const mrzOcr = require('./internal/mrzOcr');
-const symbols = require('./internal/symbols'); // SYMBOLS MRZ NUMBERS
+// const symbols = require('./internal/symbols'); // SYMBOLS MRZ NUMBERS
 const roiOptions = require('./roiOptions');
 
-const fingerprintOptions = {
-  baseDir: join(__dirname, '../fontData'),
-  height: 12,
-  width: 12,
-  minSimilarity: 0.5,
-  fontName: 'ocrb',
-  category: symbols.label,
-  ambiguity: true
-};
+// const fingerprintOptions = {
+//   baseDir: join(__dirname, '../fontData'),
+//   height: 12,
+//   width: 12,
+//   minSimilarity: 0.5,
+//   fontName: 'ocrb',
+//   category: symbols.label,
+//   ambiguity: true
+// };
 
+// const { loadFontData } = require('ocr-tools');
 /* eslint-disable no-unused-vars */
-var fontFingerprint;
-if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
-  fontFingerprint = require('../fontData/12x12/mrz/ocrb.json');
-} else {
-  // use a variable for the module name so that browserify does not include it
-  var _module = 'ocr-tools';
-  const { loadFontData } = require(_module);
-  fontFingerprint = loadFontData(fingerprintOptions);
-}
+// var fontFingerprint = loadFontData(fingerprintOptions);
 /* eslint-enable no-unused-vars */
 
 async function readMrz(image, options = {}) {
